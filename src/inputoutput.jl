aad3a9974bf9bd86571abad408bbfe1d03b8c591
Fredrik Bagge Carlson <cont-frb@ulund.org>
2021-11-03 12:29:21 +0100

    add functions to handle inputs and outputs

---
+++ src/inputoutput.jl
@@ -0,0 +1,105 @@
+using Symbolics: get_variables
+"""
+    inputs(sys)
+
+Return all variables that mare marked as inputs. See also [`unbound_inputs`](@ref)
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref)
+"""
+function inputs(sys)
+    x = states(sys)
+    filter(isinput, x)
+end
+
+"""
+    outputs(sys)
+
+Return all variables that mare marked as outputs. See also [`unbound_outputs`](@ref)
+See also [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+function outputs(sys)
+    x = states(sys)
+    filter(isoutput, x)
+end
+
+"""
+    bound_inputs(sys)
+
+Return inputs that are bound within the system, i.e., internal inputs
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref), [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+bound_inputs(sys) = filter(x->is_bound(sys, x), inputs(sys))
+
+"""
+    unbound_inputs(sys)
+
+Return inputs that are not bound within the system, i.e., external inputs
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref), [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+unbound_inputs(sys) = filter(x->!is_bound(sys, x), inputs(sys))
+
+"""
+    bound_outputs(sys)
+
+Return outputs that are bound within the system, i.e., internal outputs
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref), [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+bound_outputs(sys) = filter(x->is_bound(sys, x), outputs(sys))
+
+"""
+    unbound_outputs(sys)
+
+Return outputs that are not bound within the system, i.e., external outputs
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref), [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+unbound_outputs(sys) = filter(x->!is_bound(sys, x), outputs(sys))
+
+"""
+    is_bound(sys, u)
+
+Determine whether or not input/output variable `u` is "bound" within the system, i.e., if it's to be considered internal to `sys`.
+A variable/signal is considered bound if it appears in an equation together with variables from other subsystems.
+The typical usecase for this function is to determine whether the input to an IO component is connected to another component,
+or if it remains an external input that the user has to supply before simulating the system. 
+
+See also [`bound_inputs`](@ref), [`unbound_inputs`](@ref), [`bound_outputs`](@ref), [`unbound_outputs`](@ref)
+"""
+function is_bound(sys, u)
+    eqs = equations(sys)
+    eqs = filter(eq->has_var(eq, u), eqs) # Only look at equations that contain u
+    for eq in eqs
+        vars = [get_variables(eq.rhs); get_variables(eq.lhs)]
+        for var in vars
+            var === u && continue
+            if get_namespace(u) != get_namespace(var)
+                return true
+            end
+        end
+    end
+    false
+end
+
+"""
+    get_namespace(x)
+
+Return the namespace of a variable as a string. If the variable is not namespaced, the string is empty.
+"""
+function get_namespace(x)
+    sname = string(x)
+    parts = split(sname, '₊')
+    if length(parts) == 1
+        return ""
+    end
+    join(parts[1:end-1], '₊')
+end
+
+"""
+    has_var(eq, x)
+
+Determine whether or not an equation or expression contains variable `x`.
+"""
+function has_var(eq::Equation, x)
+    has_var(eq.rhs, x) || has_var(eq.lhs, x)
+end
+
+has_var(ex, x) = x ∈ Set(get_variables(ex))
+
